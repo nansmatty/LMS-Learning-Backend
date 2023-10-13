37b2eb6db8ef7da759c546eb6d7d99a30752e3cf
Narayan Maity <narayanmaitysp1997@gmail.com>
2023-10-13 17:04:21 +0530

    first commit

---
+++ app.ts
@@ -0,0 +1,32 @@
+import express, { NextFunction, Request, Response } from "express";
+require("dotenv").config();
+export const app = express();
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import morgan from "morgan";
+
+app.use(express.json({ limit: "50mb" }));
+
+app.use(cookieParser());
+
+app.use(cors({ origin: process.env.ORIGIN, credentials: true }));
+
+//morgan
+if (process.env.NODE_ENV === "development") {
+	app.use(morgan("dev"));
+}
+
+//testing api
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+	res.status(200).json({
+		success: true,
+		message: "API is working",
+	});
+});
+
+//unknown route
+app.all("*", (req: Request, res: Response, next: NextFunction) => {
+	const err = new Error(`Route ${req.originalUrl} not found`) as any;
+	err.statusCode = 404;
+	next(err);
+});
